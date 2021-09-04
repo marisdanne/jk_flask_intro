d0f5b1d4d393cc08d227da112ea1951413ac1a19
maris.danne <maris.danne@gmail.com>
2021-09-04 11:46:09 +0300

    add home template

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Sveika, sestdiena!!!"
+    return render_template('home.html')
 
 @app.route('/kontakti')
 def kontakti():

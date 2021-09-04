4012ff28adc011938f34e87f1cb1abe8f3acb2ce
maris.danne <maris.danne@gmail.com>
2021-09-04 11:36:22 +0300

    flask basic

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Sveika, sestdiena!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Sveika, sestdiena!!!"
+
+@app.route('/kontakti')
+def kontakti():
+    return "<html><h1>Kontakti</h1><p>Māris Danne</p></html>"
+
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
